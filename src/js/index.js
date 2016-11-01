ee792b6ced64a066d8811cceeac0a8cd41c82533
yaocs <yao.cs@qq.com>
2016-11-01 18:26:16 +0800

    jq写法 更新JS部分

--- src/js/index.js
+++ src/js/index.js
@@ -13,17 +13,42 @@ $(function () {
     }
   });
 
+  // 定位
+  var map, citysearch;
+  //加载地图，调用浏览器定位服务
+  map = new AMap.Map('container1');
+  map.plugin('AMap.CitySearch', function () {
+    citysearch = new AMap.CitySearch();
+    //自动获取用户IP，返回当前城市
+    citysearch.getLocalCity();
+    AMap.event.addListener(citysearch, "complete", function (result) {
+      if (result && result.city && result.bounds) {
+        var cityinfo = result.city;
+        var citybounds = result.bounds;
+        document.getElementById('curCity').innerHTML = cityinfo;
+        console.log(cityinfo);
+        //地图显示当前城市
+        map.setBounds(citybounds);
+      } else {
+        // document.getElementById('result').innerHTML = "您当前所在城市：" + result.info + "";
+        console.log("您当前所在城市：" + result.info + "");
+      }
+    });
+    AMap.event.addListener(citysearch, "error", function (result) {
+      alert(result.info);
+    });
+  });
+
   // 显示地区选择菜单
-  // $('#selectArea').on('click', function () {
-  //   if ($('#areaList').hasClass('hide')) {
-  //     $('#areaList').removeClass('hide').addClass('show');
-  //   }
-  // });
+  $('#selectArea').on('click', function () {
+    if ($('#areaList').hasClass('hide')) {
+      $('#areaList').removeClass('hide').addClass('show');
+    }
+  });
 
   // 选择地区
-  // $('#areaList li').on('click', function () {
-  //   $('#selectArea span').text($(this).text());
-  //   $('#areaList').removeClass('show').addClass('hide');
-  // });
+  $('#areaList li').on('click', function () {
+    $('#selectArea span').text($(this).text());
+    $('#areaList').removeClass('show').addClass('hide');
+  });
 });
-
--- src/js/search.js
+++ src/js/search.js
@@ -3,8 +3,38 @@ $(function () {
   filterShow('price');
   filterShow('screen');
   filterShow('sort');
+
+
+  $('.filter-list-type-left').find('ul li').on('click', function () {
+    var self = $(this);
+    self.addClass('active');
+    self.siblings('li').removeClass('active');
+  });
+
+  selectFilter('right');
+  selectFilter('mid');
+
+  // 过滤-功能过滤
+  $('.filter-list-top-sub li').on('click', function () {
+    if (!$(this).hasClass('active')) {
+      $(this).addClass('active');
+    } else {
+      $(this).removeClass('active');
+    }
+  });
+  // 过滤-重置功能过滤已选条件
+  $('#reset').on('click', function () {
+    $('.filter-list-top-sub li').removeClass('active');
+  });
+
+  $('#confirm').on('click', function () {
+    $('.filter-list-screen').addClass('hide').removeClass('show');
+    $('.filter-bar-screen').removeClass('active');
+    $('.filter-bar-screen').find('i').text('\ue613');
+  });
 });
 
+// 过滤条件框显示/隐藏
 function filterShow(type) {
   $('.filter-bar-'+ type).on('click', function () {
     var self = $(this);
@@ -22,3 +52,21 @@ function filterShow(type) {
     }
   });
 }
+
+function selectFilter(type) {
+  $('.filter-list-type-'+ type).find('ul li').on('click', function () {
+    var self = $(this);
+    self.addClass('active');
+    self.find('i').addClass('show').removeClass('hide');
+    self.siblings('li').removeClass('active');
+    self.siblings('li').find('i').removeClass('show').addClass('hide');
+  });
+}
+
+
+
+
+
+
+
+
